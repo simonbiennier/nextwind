a48c5ac812c0c5cb4ccfbb2e0022b861d3b31c62
simonbiennier <simon@biennier.fr>
2023-06-07 12:59:48 +0200

    audit naming

--- name.ps1
+++ name.ps1
@@ -1,9 +1,7 @@
 $files = @("src/constant/meta.ts", "package.json")
 $json = Get-Content "package.json" -Raw | ConvertFrom-Json
 $from = $json.name
-echo $from
 $to = $args[0]
-echo $to
 
 foreach ($f in $files) {
   $fileContent = Get-Content $f -Raw
