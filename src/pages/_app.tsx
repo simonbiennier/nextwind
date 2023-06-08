f21298d3edf62ec8c9061642d8380ef4a8ccf450
simonbiennier <simon@biennier.fr>
2023-06-08 10:57:29 +0200

    remove comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,24 +4,12 @@ import "@/styles/globals.css";
 
 import Layout from "@/components/layout/Layout";
 
-// import axiosClient from "@/lib/axios";
-
-// import DismissableToast from "@/components/DismissableToast";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <DismissableToast />
-
-      <SWRConfig
-        value={{
-          fetcher: (url) => axiosClient.get(url).then((res) => res.data),
-        }}
-      > */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      {/* </SWRConfig> */}
     </>
   );
 }
